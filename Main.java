c4408e7061a27ddc39f357906ba2e344a03d9642
Dan <danielknight921@gmail.com>
2019-03-10 17:13:42 +0000

    Add files via upload
    Main class

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("unused")
+		Reversi r = new Reversi();
+	}
+}
